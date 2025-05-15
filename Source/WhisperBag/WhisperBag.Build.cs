194f14fa93320eab1939ba3fd2d7a0b2b1e57f54
niscolas <niscolas@proton.me>
2025-05-14 23:46:00 -0300

    misc: initial project files

---
+++ Source/WhisperBag/WhisperBag.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class WhisperBag : ModuleRules
+{
+	public WhisperBag(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
